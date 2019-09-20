f1972a7dbb501f7dafc906b3af25e3fef11c6df8
timestee <wanghuidev@gmail.com>
2019-09-21 00:19:01 +0800

    lock_os_thread test

---
+++ lock_os_thread/lock_os_thread_test.go
@@ -0,0 +1,39 @@
+package lock_os_thread_test
+
+import (
+	"runtime"
+	"sync"
+	"testing"
+)
+
+var c1 = make(chan int)
+var c2 = make(chan int)
+
+func init() {
+	var wg sync.WaitGroup
+	wg.Add(2)
+	go func() {
+		wg.Done()
+		for range c1 {
+		}
+	}()
+	go func() {
+		wg.Done()
+		runtime.LockOSThread()
+		for range c2 {
+		}
+	}()
+	wg.Wait()
+}
+
+func Benchmark_Normal(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		c1 <- i
+	}
+}
+
+func Benchmark_LockThread(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		c2 <- i
+	}
+}
